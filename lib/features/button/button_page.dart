3050ab519f89e5dc41a76418f982194065d8e81e
henryyiu <henryyiu@hket.com>
2025-11-21 17:19:42 +0800

    Add PpCartoonButton widget and integrate it into ButtonPage; implement color utility for button styling

--- lib/features/button/button_page.dart
+++ lib/features/button/button_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:auto_route/auto_route.dart';
 
 import 'package:playground_flutter/pp_ui/widgets/app_bar/pp_app_bar.dart';
+import 'package:playground_flutter/pp_ui/widgets/button/pp_cartoon_button.dart';
 
 @RoutePage(name: "ButtonPageRoute")
 class ButtonPage extends StatefulWidget {
@@ -15,15 +16,22 @@ class ButtonPage extends StatefulWidget {
 class _ButtonPageState extends State<ButtonPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: PpAppBar(type: .back, title: 'Button'),
+    return Scaffold(
+      appBar: const PpAppBar(type: .back, title: 'Button'),
       body: SingleChildScrollView(
         padding: const .symmetric(vertical: 16, horizontal: 8),
-        child: Center(child: Wrap(spacing: 8, runSpacing: 8, children: [
-              
-
+        child: Center(
+          child: Wrap(
+            spacing: 8,
+            runSpacing: 8,
+            children: [
+              PpCartoonButton(
+                backgroundColor: Colors.lightGreen,
+                child: const Icon(Icons.star, size: 48),
+              ),
             ],
-          )),
+          ),
+        ),
       ),
     );
   }
---
+++ lib/pp_ui/utils/pp_color_util.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class PpColorUtil {
+  const PpColorUtil._();
+
+  static Color darken(Color color, [double amount = .1]) {
+    final hsl = HSLColor.fromColor(color);
+    final hslDark = hsl.withLightness((hsl.lightness - amount).clamp(0.0, 1.0));
+    return hslDark.toColor();
+  }
+}
